351cb5f99329f96d715485a68026fcb39343075b
Heesoo Home <Heesoo@Heesoo-PC>
2016-04-03 18:37:06 -0700

    First

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    //Nathan is a noob
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
